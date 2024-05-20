01a076de49f7fe2897e32139387417faf6353f7a
Muthu Selvam <muthu.softwar@gmail.com>
2024-05-19 23:21:04 -0400

    Added app.jsx code to load the remote file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+
 import { defineConfig } from 'vite'
+import federation from '@originjs/vite-plugin-federation'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'hostApp',
+      remotes: {
+        remoteApp: 'http://localhost:3000/remoteEntry.js',
+      },
+      shared: ['react','react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
